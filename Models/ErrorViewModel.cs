8d6aec2e7fff5bfe84375acfa2089a1f4b03ac52
Natalia Omiotek <natalia.omiotek15@gmail.com>
2024-04-01 10:55:53 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gry_Słownikowe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
