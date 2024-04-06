0bf459bd2c08cb555e6271bd09322a11450c3786
Natalia Omiotek <natalia.omiotek15@gmail.com>
2024-04-06 23:06:00 +0200

    sjp api done

---
+++ Entities/Wisielec.cs
@@ -0,0 +1,18 @@
+﻿using Gry_Słownikowe.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gry_Słownikowe.Entions
+{
+    public class Wisielec
+    {
+        public int Id { get; set; }
+        public string Nick { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Ranks { get; set; }
+
+    }
+}
