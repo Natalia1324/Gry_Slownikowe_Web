988ba351f5de5c7169f8cd5f27e8782c09e1d9d2
Kanra <149872920+Kanra11@users.noreply.github.com>
2024-05-03 09:44:56 +0200

    ZgadywankiPodłączenieAPI

---
+++ Models/ZgadywankiModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Gry_Słownikowe.Models
+{
+    public class ZgadywankiModel
+    {
+        public string slowo { get; set; }
+
+        public ZgadywankiModel(string slowo)
+        {
+            this.slowo = slowo;
+        }
+    }
+}
